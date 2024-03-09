da59c6bf9bc85b176664e869d5adb4d4001a1540
widger <scottwidger@gmail.com>
2024-03-09 19:46:11 +0000

    Web Authoring Personal Project Initial Commit

---
+++ script/hollowHyperScript.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+    function applyImageHoverEffect(element) {
+
+        function isElementFullyInViewport() { //checks if element fully in viewpoint
+            const rect = element.getBoundingClientRect();
+            const windowHeight = window.innerHeight;
+            const windowWidth = window.innerWidth;
+
+            return ( 
+                rect.top >= 0 &&
+                rect.bottom <= windowHeight &&
+                rect.left >= 0 &&
+                rect.right <= windowWidth
+            );
+        }
+
+        //Animation trigger to unapply greyscale
+        document.addEventListener('scroll', () => {
+            if (isElementFullyInViewport()) {
+                element.style.setProperty('--_p', '75%');
+                element.style.filter = 'grayscale(0%)';
+            } else {
+                element.style.setProperty('--_p', '25%');
+                element.style.filter = 'grayscale(100%)';
+            }
+        });
+    }
+
+    applyImageHoverEffect(document.getElementById('mantislords'));
+    applyImageHoverEffect(document.getElementById('oom'));
+    applyImageHoverEffect(document.getElementById('map'));
+    applyImageHoverEffect(document.getElementById('hyper1'));
+
+});
---
+++ script/myFavScript.js
@@ -0,0 +1,70 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const track = document.getElementById("image-track");
+    const images = track.getElementsByClassName("images");
+    const imageName = document.getElementById("imageName");
+    const imagePercentage = [
+        [-100, -83.333, "Hyper Light Drifter", "Colour", "#ca3cca"],
+        [-83.333, -66.666, "FTL: Faster Than Light", "Soundtrack", "#EC4343 "],
+        [-66.666, -50, "N++", "Gravity", "#1CE7B5 "],
+        [-50, -33.333, "Elden Ring", "Character Design", "#BDBEB5 "],
+        [-33.333, -16.666, "Hollow Knight", "Enviormental Storytelling", "#35B83D"],
+        [-16.666, 0, "Disco Elysium", "Writing", "#F5B407"],
+      ];
+
+
+    window.onmousedown = e => {//tracking when mouse is clicked for start of slider
+    track.dataset.mouseDownAt = e.clientX;  
+    }
+
+    window.onmouseup = () => {//tracks the position of the slider when you let go of mouse so it dosent reset
+    track.dataset.mouseDownAt = "0";
+    track.dataset.prevPercentage = track.dataset.percentage; 
+    }
+
+    window.onmousemove = e => {  //Moves and animates the image track with mouse drag
+        if(track.dataset.mouseDownAt === "0") return; //check if mouse clicked
+
+        const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+            maxDelta = window.innerWidth / 2;//relative position
+
+        const percentage =(mouseDelta / maxDelta) * -100, //percentage of scroll used
+            nextPercentage = parseFloat(track.dataset.prevPercentage) + percentage;
+            
+        const minPercentage = -100; //limits scroll to a range
+        const maxPercentage = 10;
+        const clampedPercentage = Math.min(Math.max(nextPercentage, minPercentage), maxPercentage);
+        track.dataset.percentage = clampedPercentage;
+    
+        track.animate(
+            { transform: `translate(${clampedPercentage}%, -50%)`},
+            { duration: 1200, fill: "forwards"}
+        );
+        
+
+        for(const image of track.getElementsByClassName("image")){ //making the images move within their frame using scroll percentage
+            image.animate(
+                {objectPosition: `${clampedPercentage + 100}% 50%`}, 
+                {duration: 1200, fill: "forwards"} 
+            );
+        }
+
+        // Update image name based on clamped percentage
+        const pair = findFloatPair(clampedPercentage, imagePercentage);
+        if (pair) {
+            imageName.textContent = pair[2]; // Display the corresponding image name
+            gameAspect.textContent = pair[3];
+            gameAspect.style.color = pair[4];
+        }
+    };
+
+    const findFloatPair = (x, pairs) => {  //checking % to see if the center of the screen is within a image
+        for (let i = 0; i < pairs.length; i++) {
+            const pair = pairs[i];
+            if (x >= pair[0] && x <= pair[1]) {
+                return pair;
+            }
+        }
+        return null;
+      };        
+
+});
